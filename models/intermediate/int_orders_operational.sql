d3e344bc71454a5f17aa8f453a81cbb99d41b7b7
Leen <leenalshehri215@gmail.com>
2024-03-06 08:19:28 +0000

    commit

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -23,27 +23,3 @@ ORDER BY orders_id DESC
 
 
 
-models
-intermediate
-int_orders_operational.sql
-
-Save
-897101112131415654231
-  ,s.ship_cost
-FROM {{ref("int_orders_margin")}} o
-LEFT JOIN {{ref("stg_raw__ship")}} s 
-  USING(orders_id)
-ORDER BY orders_id desc
-DAG showing model.my_new_project.int_orders_operational and 4 other nodes
-
-
-Enter DAG selector
-2+int_orders_operational+2
-Update Graph
-
-Enter dbt command
-dbt run
-
-} s 
-  USING(orders_id)
-ORDER BY orders_id desc
