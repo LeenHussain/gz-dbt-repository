0f7bb21f95b5f612d34182fcc96f4b92357ab130
Leen <leenalshehri215@gmail.com>
2024-03-06 08:12:35 +0000

    commit

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,15 +1,50 @@
+
 SELECT
-  o.orders_id
-  ,o.date_date
-  ,ROUND(s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin 
-  ,o.quantity
-  ,o.revenue
-  ,o.purchase_cost
-  ,o.margin
-  ,s.shipping_fee
-  ,s.logcost
+  o.orders_id,
+  o.date_date,
+  ROUND(o.margin + s.shipping_fee - (s.logcost + cast(s.ship_cost as float64)), 2) AS operational_margin,
+  o.quantity,
+  o.revenue,
+  o.purchase_cost,
+  o.margin,
+  s.shipping_fee,
+  s.logcost,
+  s.ship_cost
+FROM {{ref("int_orders_margin")}} o
+LEFT JOIN {{ref("stg_raw__ship")}} s 
+  USING (orders_id)
+ORDER BY orders_id DESC;
+
+
+
+
+
+
+
+
+
+
+models
+intermediate
+int_orders_operational.sql
+
+Save
+897101112131415654231
   ,s.ship_cost
 FROM {{ref("int_orders_margin")}} o
 LEFT JOIN {{ref("stg_raw__ship")}} s 
+  USING(orders_id)
+ORDER BY orders_id desc
+DAG showing model.my_new_project.int_orders_operational and 4 other nodes
+
+
+Enter DAG selector
+2+int_orders_operational+2
+Update Graph
+
+Enter dbt command
+dbt run
+
+} s 
   USING(orders_id)
 ORDER BY orders_id desc
